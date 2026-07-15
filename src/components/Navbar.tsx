a05e897b0ba9e72773df54eb6d97a3ce4f8887e3
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-07-15 15:47:14 +0300

    version 3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav
       data-cy="nav"
@@ -27,7 +29,10 @@ export const Navbar = () => {
                 ? 'navbar-item has-background-grey-lighter'
                 : 'navbar-item'
             }
-            to="/people"
+            to={{
+              pathname: '/people',
+              search: location.search,
+            }}
           >
             People
           </NavLink>
